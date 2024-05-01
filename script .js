6cbffba0a47e9614072fadf0ad8d051b181d9dd4
AntoninoPalmaEsposito <tonyaccacciadite@gmail.com>
2024-05-01 17:09:55 +0200

    s

--- script .js
+++ script .js
@@ -1,102 +1,102 @@
 const parole = [
-"salame", 
-"catamarano",
-"bamba",
-"poltiglia",
-"bicicletta",
-"cappuccino",
-"gelato",
-"pizza",
-"pasta",
-"caffè", 
-"tiramisù", 
-"panettone",
-"focaccia",
-"spaghetti",
-"formaggio",
-"lasagna",
-"risotto",
-"gnocchi",
-"espresso",
-"prosciutto", 
-"mozzarella", 
-"limoncello",
-"cannoli",
-"biscotti",
-"ciabatta",
-"pesto",
-"tortellini",
-"cappelletti",
-"amaretto",
-"grappa", 
-"panna", 
-"cioccolato",
-"vino",
-"tramezzino",
-"bruschetta",
-"minestrone",
-"polenta",
-"tartufo",
-"frittata",
-"cacciatorini", 
-"focaccine", 
-"ciambella",
-"crostata",
-"cavatelli",
-"tagliatelle",
-"orecchiette",
-"crespelle",
-"cassata",
-"gelatina",
-"biscotto", 
-"cannelloni", 
-"cappelletti",
-"cavallucci",
-"ciambellone",
-"cioccolatino",
-"cornetto",
-"croissant",
-"fagiolini",
-"frittelle",
-"gorgonzola", 
-"grissini", 
-"lasagnette",
-"linguine",
-"mascarpone",
-"merendina",
-"muffin",
-"nocciola",
-"panettino",
-"parmigiano",
-"pasticcini", 
-"pizzette", 
-"polpettone",
-"ravioli",
-"ricciarelli",
-"salsiccia",
-"sbrisolona",
-"scamorza",
-"scrocchiarelle",
-"semifreddo",
-"sformato", 
-"sorbetto", 
-"stracciatella",
-"strudel",
-"tiramisu",
-"tortelloni",
-"tortine",
-"trancio",
-"trentino",
-"trippa",
-"tubetti", 
-"tuffo", 
-"tuttologo",
- "ubriaco",
- "uovo",
- "valdostano",
- "vaporiera",
- "vermicelli",
- "vitello"];
+  "salame",
+  "catamarano",
+  "bamba",
+  "poltiglia",
+  "bicicletta",
+  "cappuccino",
+  "gelato",
+  "pizza",
+  "pasta",
+  "caffè",
+  "tiramisù",
+  "panettone",
+  "focaccia",
+  "spaghetti",
+  "formaggio",
+  "lasagna",
+  "risotto",
+  "gnocchi",
+  "espresso",
+  "prosciutto",
+  "mozzarella",
+  "limoncello",
+  "cannoli",
+  "biscotti",
+  "ciabatta",
+  "pesto",
+  "tortellini",
+  "cappelletti",
+  "amaretto",
+  "grappa",
+  "panna",
+  "cioccolato",
+  "vino",
+  "tramezzino",
+  "bruschetta",
+  "minestrone",
+  "polenta",
+  "tartufo",
+  "frittata",
+  "cacciatorini",
+  "focaccine",
+  "ciambella",
+  "crostata",
+  "cavatelli",
+  "tagliatelle",
+  "orecchiette",
+  "crespelle",
+  "cassata",
+  "gelatina",
+  "biscotto",
+  "cannelloni",
+  "cappelletti",
+  "cavallucci",
+  "ciambellone",
+  "cioccolatino",
+  "cornetto",
+  "croissant",
+  "fagiolini",
+  "frittelle",
+  "gorgonzola",
+  "grissini",
+  "lasagnette",
+  "linguine",
+  "mascarpone",
+  "merendina",
+  "muffin",
+  "nocciola",
+  "panettino",
+  "parmigiano",
+  "pasticcini",
+  "pizzette",
+  "polpettone",
+  "ravioli",
+  "ricciarelli",
+  "salsiccia",
+  "sbrisolona",
+  "scamorza",
+  "scrocchiarelle",
+  "semifreddo",
+  "sformato",
+  "sorbetto",
+  "stracciatella",
+  "strudel",
+  "tiramisu",
+  "tortelloni",
+  "tortine",
+  "trancio",
+  "trentino",
+  "trippa",
+  "tubetti",
+  "tuffo",
+  "tuttologo",
+  "ubriaco",
+  "uovo",
+  "valdostano",
+  "vaporiera",
+  "vermicelli",
+  "vitello"];
 
 let letteraA = document.getElementById("a");
 let letteraB = document.getElementById("b");
@@ -124,6 +124,7 @@ let letteraW = document.getElementById("w");
 let letteraX = document.getElementById("x");
 let letteraY = document.getElementById("y");
 let letteraZ = document.getElementById("z");
+
 const parolaContainer = document.getElementById("parolaUser");
 const parolaFalso = document.getElementById("falso");
 const cancella = document.getElementById("cancella");
@@ -139,7 +140,10 @@ let punteggio = 0;
 let array = [];
 let arrayp = [];
 
+
+
 function getParola() {
+
   let numero = Math.floor(Math.random() * parole.length);
   parolaDaIndovinare.textContent = parole[numero];
   parolaDaIndovinare.textContent.split("").forEach((lettera) => {
@@ -149,24 +153,26 @@ function getParola() {
   });
 }
 
+
+
 function gestisciClick(lettera) {
+
   lettera.addEventListener("click", function () {
     const letteraCliccata = lettera.textContent.toLowerCase();
     const parolaSegreta = parolaDaIndovinare.textContent;
     const lettereParola = parolaSegreta.split("");
-    
-console.log(lettereParola);
-    if (array.includes(letteraCliccata)){
-    console.log('hey');
+
+
+    if (array.includes(letteraCliccata)) {
+
     } else if (lettereParola.includes(letteraCliccata)) {
       lettereParola.forEach((letteraParola, index) => {
+        
         if (letteraParola === letteraCliccata) {
           parolaContainer.children[index].textContent = letteraCliccata;
-          
           array.push(letteraCliccata);
-          console.log(array);
+
           if (confrontoArray(lettereParola, array)) {
-            console.log('ciaoo');
             immagine.src = "assets/vittoria.png";
             tastieraContainer.classList.add("opacity-0");
             setTimeout(() => {
@@ -175,18 +181,18 @@ console.log(lettereParola);
           }
         }
       });
+
       lettera.classList.add("rilievo3");
+
     } else if (arrayp.includes(letteraCliccata)) {
-          console.log('lettera già cliccata')
-    }
-    
-      else {
+
+    } else {
       arrayp.push(letteraCliccata);
       lettera.classList.add("rilievo4");
       punteggio++;
       let risultato = 6 - punteggio;
       vite.textContent = risultato;
-      console.log(risultato);
+
       if (risultato == 0) {
         parolaContainer.classList.remove("flex");
         parolaContainer.classList.add("hidden");
@@ -195,13 +201,13 @@ console.log(lettereParola);
 
         immagine.src = "assets/errore6.png";
         tastieraContainer.classList.add("opacity-0");
+
         setTimeout(() => {
           immagine.src = "assets/perso.png";
         }, 500);
         setTimeout(() => {
           location.reload();
         }, 6000);
-
 
       } else if (risultato == 1) {
         vite.style.color = "rgb(220, 79, 82)";
